f51774dce360b3e0b3d440d7685c9b79c8ca270f
magnunm <magnusnym97@gmail.com>
2022-02-20 14:32:31 +0100

    Extract URL parsing to module

--- app/Main.hs
+++ app/Main.hs
@@ -1,15 +1,13 @@
 module Main where
 
 import System.Environment (getArgs)
-import Data.List (inits, tails, isPrefixOf, isSuffixOf)
-import Data.Either (isLeft, fromRight)
 import Data.Aeson ((.=), object, Value)
 import qualified Data.Aeson.Key as Key
-import Data.Text (pack, Text)
 import Data.Text.Encoding (decodeUtf8)
 import qualified Data.Text.IO as T.IO
 import qualified Network.HTTP.Req as Req
-import Network.HTTP.Req ((/:), Scheme( Https, Http ))
+
+import Url (convertUrl)
 
 main :: IO ()
 main = do
@@ -39,54 +37,6 @@ request httpMethod url
   | httpMethod == "POST" = postJSON url payload
   | otherwise = error "FIXME"
 
-newtype UrlParseError = UrlParseError String
-  deriving Show
-
-convertUrl
-  :: String
-     -> Either
-          UrlParseError
-          (Either (Req.Url 'Http) (Req.Url 'Https))
-convertUrl url = do
-  untilPath <- hostAndScheme url
-  hostAndPath <- case fromSequence "://" url of
-    Just hostAndPath -> Right hostAndPath
-    Nothing -> Left noSchemeSeparatorError
-  let hostAndPathSegments = splitPath hostAndPath
-  if length hostAndPathSegments < 2
-    then Right untilPath
-    else case untilPath of
-      Left url -> Right (Left (foldl (/:) url (tail hostAndPathSegments)))
-      Right url -> Right (Right (foldl (/:) url (tail hostAndPathSegments)))
-
-splitPath :: String -> [Text]
-splitPath "" = []
-splitPath url = case length fromSlash of
-    0 -> [pack untilSlash]
-    1 -> [pack untilSlash, pack ""] -- Ends in forward slash
-    _ -> pack untilSlash : splitPath (tail fromSlash)
-  where (untilSlash, fromSlash) = break (== '/') url
-
-hostAndScheme
-  :: String
-     -> Either
-          UrlParseError
-          (Either (Req.Url 'Http) (Req.Url 'Https))
-hostAndScheme url = do
-  host <- getHost url
-  case untilSequence "://" url of
-    Just "http" -> Right (Left (Req.http host))
-    Just "https" -> Right (Right (Req.https host))
-    Just scheme -> Left (UrlParseError ("Unsupported scheme: " ++ scheme))
-    Nothing -> Left noSchemeSeparatorError
-
-getHost :: String -> Either UrlParseError Text
-getHost url = case fromSequence "://" url of
-  Just hostAndPath -> Right (head (splitPath hostAndPath))
-  Nothing -> Left noSchemeSeparatorError
-
-noSchemeSeparatorError = UrlParseError "Could not find `://` in URL"
-
 payload = object
   [ Key.fromString "foo" .= "bar"
   ]
@@ -102,17 +52,3 @@ postJSON url body =
 get :: (Req.MonadHttp m) => Req.Url scheme -> m Req.BsResponse
 get url =
     Req.req Req.GET url Req.NoReqBody Req.bsResponse mempty
-
-untilSequence :: (Eq a) => [a] -> [a] -> Maybe [a]
-untilSequence sequence original = if null matchingInits
-  then Nothing
-  else Just (take
-              (length (head matchingInits) - length sequence)
-              (head matchingInits))
-  where matchingInits = filter (isSuffixOf sequence) (inits original)
-
-fromSequence :: (Eq a) => [a] -> [a] -> Maybe [a]
-fromSequence sequence original = if null matchingTails
-  then Nothing
-  else Just (drop (length sequence) (head matchingTails))
-  where matchingTails = filter (isPrefixOf sequence) (tails original)
---
+++ app/Url.hs
@@ -0,0 +1,68 @@
+module Url (UrlParseError, convertUrl) where
+
+import Data.List (inits, tails, isPrefixOf, isSuffixOf)
+import Data.Text (pack, Text)
+import qualified Network.HTTP.Req as Req
+import Network.HTTP.Req ((/:), Scheme( Https, Http ))
+
+newtype UrlParseError = UrlParseError String
+  deriving Show
+
+convertUrl
+  :: String
+     -> Either
+          UrlParseError
+          (Either (Req.Url 'Http) (Req.Url 'Https))
+convertUrl url = do
+  untilPath <- hostAndScheme url
+  hostAndPath <- case fromSequence "://" url of
+    Just hostAndPath -> Right hostAndPath
+    Nothing -> Left noSchemeSeparatorError
+  let hostAndPathSegments = splitPath hostAndPath
+  if length hostAndPathSegments < 2
+    then Right untilPath
+    else case untilPath of
+      Left url -> Right (Left (foldl (/:) url (tail hostAndPathSegments)))
+      Right url -> Right (Right (foldl (/:) url (tail hostAndPathSegments)))
+
+splitPath :: String -> [Text]
+splitPath "" = []
+splitPath url = case length fromSlash of
+    0 -> [pack untilSlash]
+    1 -> [pack untilSlash, pack ""] -- Ends in forward slash
+    _ -> pack untilSlash : splitPath (tail fromSlash)
+  where (untilSlash, fromSlash) = break (== '/') url
+
+hostAndScheme
+  :: String
+     -> Either
+          UrlParseError
+          (Either (Req.Url 'Http) (Req.Url 'Https))
+hostAndScheme url = do
+  host <- getHost url
+  case untilSequence "://" url of
+    Just "http" -> Right (Left (Req.http host))
+    Just "https" -> Right (Right (Req.https host))
+    Just scheme -> Left (UrlParseError ("Unsupported scheme: " ++ scheme))
+    Nothing -> Left noSchemeSeparatorError
+
+getHost :: String -> Either UrlParseError Text
+getHost url = case fromSequence "://" url of
+  Just hostAndPath -> Right (head (splitPath hostAndPath))
+  Nothing -> Left noSchemeSeparatorError
+
+noSchemeSeparatorError = UrlParseError "Could not find `://` in URL"
+
+untilSequence :: (Eq a) => [a] -> [a] -> Maybe [a]
+untilSequence sequence original = if null matchingInits
+  then Nothing
+  else Just (take
+              (length (head matchingInits) - length sequence)
+              (head matchingInits))
+  where matchingInits = filter (isSuffixOf sequence) (inits original)
+
+fromSequence :: (Eq a) => [a] -> [a] -> Maybe [a]
+fromSequence sequence original = if null matchingTails
+  then Nothing
+  else Just (drop (length sequence) (head matchingTails))
+  where matchingTails = filter (isPrefixOf sequence) (tails original)
